53aa2e36f6b7c1b47cb9513f27a41d56e1e2ffd9
RARaghavarora <raghavarora999@yahoo.in>
2019-03-21 12:14:25 +0530

    Admin site

--- polysis/admin.py
+++ polysis/admin.py
@@ -8,7 +8,7 @@ class AnswerInline(admin.StackedInline):
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['questionkey', 'content', 'score_increment', 'score_decrement']
+    fields = ['questionkey', 'content', 'score_increment', 'score_decrement', 'image', ('is_image', 'is_mcq')]
     inlines = [AnswerInline]
 
 
